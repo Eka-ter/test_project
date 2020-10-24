76b149eb407d7f5217c87ac45a7db19e30d7be18
Eka-ter <example@gmail.com>
2020-10-24 18:16:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log (second);
+
+// let person = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(person['name']);
+
+// let arr = ['plum.png','orange.png','apple.bmp'];
+// console.log(arr[0]);
+
+// alert('Hello World!');
+// let answer = confirm('Are you here?');
+// console.log(answer);
+
+// let answer = +prompt('Есть 18?', "Yes");
+// console.log(typeof(answer));
+
+// console.log('arr'+'-object');
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
